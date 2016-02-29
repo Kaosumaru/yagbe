6744f74a0887f2c3b93c48a88540269373ca8cd4
Mateusz Borycki <mateuszborycki@gmail.com>
2016-02-29 19:49:28 +0100

    added tuple_element

--- vm/utils.hpp
+++ vm/utils.hpp
@@ -10,6 +10,9 @@ namespace std
 
 	template< bool B, class T1, class T2 >
 	using conditional_t = typename std::conditional<B, T1, T2>::type;
+
+	template <std::size_t I, class T>
+	using tuple_element_t = typename std::tuple_element<I, T>::type;
 }
 
 #endif
