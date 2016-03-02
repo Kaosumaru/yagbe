f9852f587ddae58826caf2359597937a8ae32c78
Mateusz Borycki <mateuszborycki@gmail.com>
2016-03-02 15:47:27 +0100

    added memory interceptors

--- vm/memory.hpp
+++ vm/memory.hpp
@@ -1,11 +1,22 @@
 #pragma once
 #include <cstdint>
+#include <functional>
+#include <array>
 
 namespace yagbe
 {
 	class memory
 	{
 	public:
+		struct interceptor
+		{
+			using ReadCallback = std::function<uint8_t(memory &m, uint16_t)>;
+			using WriteCallback = std::function<void(memory &m, uint16_t, uint8_t)>;
+
+			ReadCallback  onRead;
+			WriteCallback onWrite;
+		};
+
 		//TODO endianess
 		union ByteWord
 		{
@@ -87,12 +98,19 @@ namespace yagbe
 
 		uint8_t read_at(uint16_t address)
 		{
-			return data[address];
+			auto& inter = interceptor_at(address).onRead;
+			if (!inter)
+				return data[address];
+			return inter(*this, address);
 		}
 
 		void write_byte_at(uint16_t address, uint8_t byte)
 		{
-			data[address] = byte;
+			auto& inter = interceptor_at(address).onWrite;
+			if (!inter)
+				data[address] = byte;
+			else
+				inter(*this, address, byte);
 		}
 
 		void write_word_at(uint16_t address, uint16_t word)
@@ -103,9 +121,17 @@ namespace yagbe
 			write_byte_at(address + 1, w.byte[1]);
 		}
 
+protected:
+		interceptor& interceptor_at(uint16_t address)
+		{
+			auto index = address >> 8;
+			return _interceptors[index];
+		}
 
-
-
+		constexpr static int interceptor_range = 0x100;
+		std::array<interceptor, interceptor_range> _interceptors;
 		uint8_t data[0xFFFF];
+		
+
 	};
 };
