baef88227b041dd964035c87fd5c1755883224d7
Mateusz Borycki <mateuszborycki@gmail.com>
2016-03-08 16:42:45 +0100

    added mbc3 handler

--- vm/rom/mbc_handler.cpp
+++ vm/rom/mbc_handler.cpp
@@ -21,7 +21,7 @@ class mbc_handler_0 : public mbc_handler
 };
 
 
-class mbc1_handler : public mbc_handler
+class generic_mbc_handler : public mbc_handler
 {
 	using mbc_handler::mbc_handler;
 
@@ -29,41 +29,9 @@ class mbc1_handler : public mbc_handler
 	{
 		if (address < rom_bank_size())
 			return _m.raw_at(address);
-		
+
 		auto offset = (int)address - rom_bank_size();
 		return *(address_of_current_bank() + offset);
-	}
-
-	void handle_write(uint16_t address, uint8_t byte) override
-	{
-		if (address < 0x2000)
-		{
-			//Enable external RAM
-			_ram_enabled = byte == 0x0A;
-		}
-		else if (address < 0x4000)
-		{
-			//ROM bank (low 5 bits)
-			update_byte_with_mask<uint8_t>(_rom_bank_temp, byte, 0b00011111);
-			select_rom_bank(_rom_bank_temp);
-		}
-		else if (address < 0x6000)
-		{
-			//ROM bank (high 2 bits) set, RAM bank
-			if (_rom_mode)
-			{
-				update_byte_with_mask<uint8_t>(_rom_bank_temp, byte << 5, 0b01100000);
-				select_rom_bank(_rom_bank_temp);
-			}
-			else
-			{
-				//NYI
-			}
-		}
-		else
-		{
-			_rom_mode = byte == 0;
-		}
 	}
 
 protected:
@@ -86,16 +54,84 @@ protected:
 		return _rom_data.data() + rom_bank_size() * _selected_rom_bank;
 	}
 
-	bool _rom_mode = true;
-
 	uint8_t _selected_rom_bank = 1;
+};
+
+class mbc1_handler : public generic_mbc_handler
+{
+	using generic_mbc_handler::generic_mbc_handler;
+
+	void handle_write(uint16_t address, uint8_t byte) override
+	{
+		if (address < 0x2000) //Enable external RAM
+		{
+			_ram_enabled = byte == 0x0A;
+		}
+		else if (address < 0x4000) 	//ROM bank (low 5 bits)
+		{
+			update_byte_with_mask<uint8_t>(_rom_bank_temp, byte, 0b00011111);
+			select_rom_bank(_rom_bank_temp);
+		}
+		else if (address < 0x6000)	//ROM bank (high 2 bits) set OR RAM bank
+		{
+			if (_rom_mode)
+			{
+				update_byte_with_mask<uint8_t>(_rom_bank_temp, byte << 5, 0b01100000);
+				select_rom_bank(_rom_bank_temp);
+			}
+			else
+			{
+				//NYI
+			}
+		}
+		else
+		{
+			_rom_mode = byte == 0;
+		}
+	}
+
+protected:
+	bool _rom_mode = true;
+	
 	uint8_t _rom_bank_temp = 0;
 
 	bool _ram_enabled = false;
 	int _ram_bank = 0;
-
 };
 
+
+
+class mbc3_handler : public generic_mbc_handler
+{
+	using generic_mbc_handler::generic_mbc_handler;
+
+	void handle_write(uint16_t address, uint8_t byte) override
+	{
+		if (address < 0x2000) //Enable external RAM & Timer
+		{
+			_ram_enabled = byte == 0x0A;
+		}
+		else if (address < 0x4000) 	//ROM bank
+		{
+			select_rom_bank(byte);
+		}
+		else if (address < 0x6000)	//RAM Bank Number - or -RTC Register Select(Write Only)
+		{
+			//NYI
+		}
+		else //Latch Clock Data (Write Only)
+		{
+			//NYI
+		}
+	}
+
+protected:
+
+	uint8_t _rom_bank_temp = 0;
+
+	bool _ram_enabled = false;
+	int _ram_bank = 0;
+};
 
 mbc_handler::pointer mbc_handler::create_for(rom_info *info, memory& m, std::vector<uint8_t>&& data)
 {
@@ -106,16 +142,33 @@ mbc_handler::pointer mbc_handler::create_for(rom_info *info, memory& m, std::vec
 	{
 	case 0x00: //rom only
 		return std::make_unique<mbc_handler_0>(m, std::move(data));
+
 	case 0x01: //MBC1
-		return std::make_unique<mbc1_handler>(m, std::move(data));
 	case 0x02: //MBC1+RAM
-		return std::make_unique<mbc1_handler>(m, std::move(data));
 	case 0x03: //MBC1+RAM+BATT
 		return std::make_unique<mbc1_handler>(m, std::move(data));
 
-	case 0x04: //MBC2+RAM
-	case 0x05: //MBC2+RAM+BATT
+	case 0x05: //MBC2+RAM
+	case 0x06: //MBC2+RAM+BATT
 		break;
+
+	case 0x08: //ROM+RAM
+	case 0x09: //ROM+RAM+BATT
+		return std::make_unique<mbc_handler_0>(m, std::move(data));
+
+	case 0x12: //MBC3+RAM
+	case 0x13: //MBC3+RAM+BATT
+		return std::make_unique<mbc3_handler>(m, std::move(data));
+		break;
+
+	case 0x19: //MBC5
+	case 0x1A: //MBC5+RAM
+	case 0x1B: //MBC5+RAM+BATT
+	case 0x1C: //MBC5+RUMBLE
+	case 0x1D: //MBC5+RUMBLE+SRAM
+	case 0x1E: //MBC5+RUMBLE+SRAM+BATT
+		break;
+
 	}
 
 	throw std::runtime_error("Not implemented this MBC");
