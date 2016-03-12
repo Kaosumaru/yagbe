2096c5727b743eebcc4dd5f767ebd56be25faab9
Mateusz Borycki <mateuszborycki@gmail.com>
2016-03-12 14:08:00 +0100

    optimized tile renderer

--- vm/gpu/tilemap.hpp
+++ vm/gpu/tilemap.hpp
@@ -24,12 +24,84 @@ namespace yagbe
 				_parent_tilemap.reset(new tilemap(_m, true));
 		}
 
-		int palette_at_point(const ipoint& p)
+
+		struct render_info
 		{
+			render_info() {}
+			bool visible = true;
+
+			int tile_offset;
+			int in_tile_y;
+			int in_tile_x;
+
+			int reset_tile_offset_at = 99999;
+			int reset_tile_offset_to = 0;
+
+
+			uint8_t *tilemap_pointer;
+			tile_info *tile_set_pointer;
+			int tile_set_index;
+
+			int palette_at_current_pixel() const 
+			{
+				if (!visible)
+					return -1;
+				auto info = current_tile();
+				auto c = info->palette_index_at(in_tile_x, in_tile_y);
+				return c;
+			}
+
+			void advance_x()
+			{
+				if (++in_tile_x != 8)
+					return;
+				in_tile_x = 0;
+				tile_offset++;
+
+				if (tile_offset == reset_tile_offset_at)
+					tile_offset = reset_tile_offset_to;
+			}
+		protected:
+			tile_info* current_tile() const
+			{
+				uint8_t tile_index = *(tilemap_pointer + tile_offset);
+				return tile_at_index(tile_index);
+			}
+
+
+			tile_info *tile_at_index(uint8_t i) const
+			{
+				tile_info *info = tile_set_pointer;
+
+				if (tile_set_index == 0)
+					info += (int8_t)i; //this tileset is using signed coords
+				else
+					info += i; //this tileset is using unsigned coords
+
+				return info;
+			}
+		};
+
+
+		auto tile_offset_from(const ipoint& p)
+		{
+			//auto off = offset();
+			int pix_x = p.x;// +off.x;
+			int pix_y = p.y;// +off.y;
+
+			int tile_x = (pix_x / tile_size().x);
+			int tile_y = (pix_y / tile_size().y);
+			return tile_x + tile_y * tilemap_size().x;
+		}
+
+		auto render_info_at_point(const ipoint& p)
+		{
+			render_info info;
+
 			auto off = offset();
 			int pix_x = p.x + off.x;
 			int pix_y = p.y + off.y;
-			
+
 			int tile_x = (pix_x / tile_size().x);
 			int tile_y = (pix_y / tile_size().y);
 
@@ -37,32 +109,52 @@ namespace yagbe
 			{
 				tile_x %= tilemap_size().x;
 				tile_y %= tilemap_size().y;
+
+				info.reset_tile_offset_to = tile_offset_from({0, p.y});
+				info.reset_tile_offset_at = tile_offset_from({ tilemap_pixel_size().x, p.y });
+
 			}
 			else
 			{
 				if (pix_x < 0 || tile_x >= tilemap_size().x)
-					return - 1;
+					info.visible = false;
 				if (pix_y < 0 || tile_y >= tilemap_size().y)
-					return - 1;
+					info.visible = false;
+
+				if (!info.visible)
+					return info;
 			}
 
-
-			int offset = tile_x + tile_y * tilemap_size().x;
-			uint8_t tile_index = _m.raw_at(current_tilemap_address() + offset);
-
-			//shows tiles as blocks
-			//uint8_t c = 10 * tile_index;
-			//return{ c,c,c,255 };
-
-			auto info = tile_at_index(tile_index);
-			auto c = info->palette_index_at(pix_x % tile_size().x, pix_y % tile_size().y);
-			return c;
+			
+			info.tile_offset = tile_x + tile_y * tilemap_size().x;
+			info.in_tile_x = pix_x % tile_size().x;
+			info.in_tile_y = pix_y % tile_size().y;
+			info.tilemap_pointer = _m.raw_pointer_at(current_tilemap_address());
+			info.tile_set_index = tileset_index();
+			info.tile_set_pointer = tileset_pointer();
+			return info;
 		}
+
+		int palette_at_render_info(const render_info& p)
+		{
+			return p.palette_at_current_pixel();
+		}
+
+		int palette_at_point(const ipoint& p)
+		{
+			auto info = render_info_at_point(p);
+			return info.palette_at_current_pixel();
+		}
+
+
+
 
 		bool render_scanline(yagbe::color* line, int line_index, int width)
 		{
 			if (!enabled())
 				return false;
+
+			auto render_info = render_info_at_point({ 0, line_index });
 
 			for (int i = 0; i < width; i++)
 			{
@@ -72,13 +164,16 @@ namespace yagbe
 					if (palette_index != -1)
 					{
 						line[i] = color_of_index(palette_index);
+
+						render_info.advance_x();
 						continue;
 					}
 				}
 
-				auto palette_index = palette_at_point({ i, line_index });
+				auto palette_index = render_info.palette_at_current_pixel();
 				if (palette_index != -1)
 					line[i] = color_of_index(palette_index);
+				render_info.advance_x();
 			}
 
 			return true;
@@ -123,24 +218,13 @@ namespace yagbe
 		}
 
 	protected:
-		tile_info *tile_at_index(uint8_t i)
+		tile_info* tileset_pointer()
 		{
-			tile_info *info;
-
 			if (tileset_index() == 0)
-			{
-				info = (tile_info*)_m.raw_pointer_at(0x9000);
-				info += (int8_t)i; //this tileset is using signed coords
-			}
-			else
-			{
-				info = (tile_info*)_m.raw_pointer_at(0x8000);
-				info += i; //this tileset is using unsigned coords
-			}
-
-
-			return info;
+				return (tile_info*)_m.raw_pointer_at(0x9000);
+			return (tile_info*)_m.raw_pointer_at(0x8000);
 		}
+
 
 		std::unique_ptr<tilemap> _parent_tilemap;
 		memory &_m;
