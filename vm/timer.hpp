36b91c938e179c9a9a6d33f2b8cf7f9edab2fcf8
Mateusz Borycki <mateuszborycki@gmail.com>
2016-03-18 15:58:28 +0100

    fixed DIV resetting

--- vm/timer.hpp
+++ vm/timer.hpp
@@ -13,16 +13,24 @@ namespace yagbe
 
 		void step(uint32_t delta_tclock, uint32_t cycles_elapsed)
 		{
+
+			_div_increments += delta_tclock;
+			while (_div_increments >= _div_speed)
+			{
+				_div_increments -= _div_speed;
+				_m.io_register.DIV++;
+			}
+
 			_m.io_register.DIV = cycles_elapsed / 256;
 
 			if (!_m.io_register.TAC_running)
 				return;
 
 			auto speed_mode = _m.io_register.TAC & 0b11;
-			_tima_speed = mode_speeds()[speed_mode];
+			auto tima_speed = mode_speeds()[speed_mode];
 			_tima_increments += delta_tclock;
 
-			while (_tima_increments >= _tima_speed)
+			while (_tima_increments >= tima_speed)
 			{
 				if (_m.io_register.TIMA == 255)
 				{
@@ -32,7 +40,7 @@ namespace yagbe
 				else
 					_m.io_register.TIMA++;
 
-				_tima_increments -= _tima_speed;
+				_tima_increments -= tima_speed;
 			}
 		}
 
@@ -43,7 +51,8 @@ namespace yagbe
 
 		void reset()
 		{
-			
+			_tima_increments = 0;
+			_div_increments = 0;
 		}
 
 	protected:
@@ -56,8 +65,10 @@ namespace yagbe
 		constexpr static std::array<int, 4> mode_speeds() { return{ 256*4, 4 * 4, 16 * 4, 64 * 4 }; }
 
 
-		int _tima_speed = 4;
 		int _tima_increments = 0;
+
+		int _div_increments = 0;
+		int _div_speed = 256;
 
 		interrupts &_i;
 		memory &_m;
