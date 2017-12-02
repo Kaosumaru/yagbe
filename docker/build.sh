37a7594cbe344b891edb58fce31de808c1dd6a81
Mateusz Borycki <mateuszborycki@gmail.com>
2017-12-03 00:29:02 +0100

    Fix scripts.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+umask 0000
 mkdir -p build
+umask 0011s
 cd build
 emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=make -G "Unix Makefiles"
 emmake make
--- docker/docker_emscripten.sh
+++ docker/docker_emscripten.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-mkdir -p build
 docker run --rm -v $(pwd):/src -t kaosumaru/emscripten:test ./docker/build.sh
