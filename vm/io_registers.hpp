256da583dd443e2a49ce6bc74fd847800dd13422
Mateusz Borycki <mateuszborycki@gmail.com>
2018-06-09 18:54:46 +0200

    Added missing sound controls.

--- vm/io_registers.hpp
+++ vm/io_registers.hpp
@@ -37,6 +37,52 @@ namespace yagbe
 			unsigned int initialize : 1; //Setting this bit to 1 restarts Sound
 		};
 
+		struct AudioWave
+		{
+			//BYTE 1
+			unsigned int : 7;
+			unsigned int enabled : 1;
+
+			//BYTE 2
+			unsigned int sound_length : 8;
+
+			//BYTE 3
+			unsigned int : 4;
+			unsigned int output_level : 2; // 0 = decrese, 1 = increase
+			unsigned int : 2;
+
+			//BYTE 4
+			unsigned int lfrequency : 8; // lower byte of frequency
+
+			//BYTE 5
+			unsigned int hfrequency : 3; // higher part of frequency
+			unsigned int : 3;
+			unsigned int counter_enabled : 1; // if 'sound off' counter enabled (will disable sound after sound_length)
+			unsigned int initialize : 1; //Setting this bit to 1 restarts Sound
+		};
+
+		struct AudioNoise
+		{
+			//BYTE 1
+			unsigned int sound_length : 6;
+			unsigned int : 2;
+
+			//BYTE 2
+			unsigned int envelope_length : 3; // length of envelope steps, N x (1/64) sec
+			unsigned int envelope_add_mode : 1; // 0 = decrese, 1 = increase
+			unsigned int envelope_default_volume : 4; // default envelope value
+
+			//BYTE 3
+			unsigned int poly_division : 3;
+			unsigned int poly_steps : 1;
+			unsigned int poly_frequency : 4;
+
+			//BYTE 4
+			unsigned int : 6;
+			unsigned int counter_enabled : 1; // if 'sound off' counter enabled (will disable sound after sound_length)
+			unsigned int initialize : 1; //Setting this bit to 1 restarts Sound
+		};
+
 
 		struct VolumeControl
 		{
@@ -78,10 +124,10 @@ namespace yagbe
 		uint8_t &SC{ _a[0xFF02] }; //X R/W SIO control
 
 		// timers NYI
-		uint8_t &DIV{ _a[0xFF04] }; // R/W Divider Register
+		uint8_t &DIV { _a[0xFF04] }; // R/W Divider Register
 		uint8_t &TIMA{ _a[0xFF05] }; // R/W Timer counter
-		uint8_t &TMA{ _a[0xFF06] }; // R/W timer modulo
-		uint8_t &TAC{ _a[0xFF07] }; // R/W timer control
+		uint8_t &TMA { _a[0xFF06] }; // R/W timer modulo
+		uint8_t &TAC { _a[0xFF07] }; // R/W timer control
 
 		bit TAC_running{ TAC, 2 };
 
@@ -91,29 +137,27 @@ namespace yagbe
 		// FF10-FF40 - music
 		AudioSquare &AUDIO_square1 { *(AudioSquare*)(_a + 0xFF10) };
 		AudioSquare &AUDIO_square2 { *(AudioSquare*)(_a + 0xFF15) };
-
-		uint8_t &FF14 {_a[0xFF14]};
-		//X sound3
-		//X sound4
+		AudioWave   &AUDIO_wave    { *(AudioWave*)  (_a + 0xFF1A) };
+		AudioNoise  &AUDIO_noise   { *(AudioNoise*) (_a + 0xFF20) };
 
 		VolumeControl &AUDIO_volume { (VolumeControl&)_a[0xFF24] };
 
 		uint8_t &AUDIO_output { _a[0xFF25] };
-		bit AUDIO_s1_to_so1 { AUDIO_output, 0 };
-		bit AUDIO_s2_to_so1 { AUDIO_output, 1 };
-		bit AUDIO_s3_to_so1 { AUDIO_output, 2 };
-		bit AUDIO_s4_to_so1 { AUDIO_output, 3 };
-		bit AUDIO_s1_to_so2 { AUDIO_output, 4 };
-		bit AUDIO_s2_to_so2 { AUDIO_output, 5 };
-		bit AUDIO_s3_to_so2 { AUDIO_output, 6 };
-		bit AUDIO_s4_to_so2 { AUDIO_output, 7 };
+		bit AUDIO_s1_to_so1   { AUDIO_output, 0 };
+		bit AUDIO_s2_to_so1   { AUDIO_output, 1 };
+		bit AUDIO_s3_to_so1   { AUDIO_output, 2 };
+		bit AUDIO_s4_to_so1   { AUDIO_output, 3 };
+		bit AUDIO_s1_to_so2   { AUDIO_output, 4 };
+		bit AUDIO_s2_to_so2   { AUDIO_output, 5 };
+		bit AUDIO_s3_to_so2   { AUDIO_output, 6 };
+		bit AUDIO_s4_to_so2   { AUDIO_output, 7 };
 
 		uint8_t &AUDIO_control { _a[0xFF26] };
-		bit AUDIO_s1_enabled { AUDIO_control, 0 };
-		bit AUDIO_s2_enabled { AUDIO_control, 1 };
-		bit AUDIO_s3_enabled { AUDIO_control, 2 };
-		bit AUDIO_s4_enabled { AUDIO_control, 3 };
-		bit AUDIO_all_enabled { AUDIO_control, 7 };
+		bit AUDIO_s1_enabled   { AUDIO_control, 0 };
+		bit AUDIO_s2_enabled   { AUDIO_control, 1 };
+		bit AUDIO_s3_enabled   { AUDIO_control, 2 };
+		bit AUDIO_s4_enabled   { AUDIO_control, 3 };
+		bit AUDIO_all_enabled  { AUDIO_control, 7 };
 
 		WaveTable AUDIO_wave_table { _a + 0xFF30 };
 
@@ -142,11 +186,11 @@ namespace yagbe
 		uint8_t &SCY{ _a[0xFF42] }; //R/W scroll y
 		uint8_t &SCX{ _a[0xFF43] };	//R/W scroll x
 
-		uint8_t &LY{ _a[0xFF44] }; //R LCDC Y-Coordinate
+		uint8_t &LY { _a[0xFF44] }; //R LCDC Y-Coordinate
 		uint8_t &LYC{ _a[0xFF45] }; //R/W LY Compare
 		uint8_t &DMA{ _a[0xFF46] }; //W DMA Transfer and Start Address 
 
-		uint8_t &BGP{ _a[0xFF47] }; //R/W BG & Window Palette Data
+		uint8_t &BGP { _a[0xFF47] }; //R/W BG & Window Palette Data
 		uint8_t &OBP0{ _a[0xFF48] }; //R/W Object Palette 0 Data
 		uint8_t &OBP1{ _a[0xFF49] }; //R/W Object Palette 1 Data
 
@@ -155,7 +199,7 @@ namespace yagbe
 
 		//cbg
 		uint8_t &KEY1{ _a[0xFF4D] }; //X cpu speed
-		uint8_t &VBK{ _a[0xFF4F] }; //X vram bank
+		uint8_t &VBK { _a[0xFF4F] }; //X vram bank
 
 		uint8_t &HDMA1{ _a[0xFF51] }; //X HDMA1
 		uint8_t &HDMA2{ _a[0xFF52] }; //X HDMA2
