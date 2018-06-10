79bf8330d7dd131847ca545fca5fac671c751568
Mateusz Borycki <mateuszborycki@gmail.com>
2018-06-10 15:58:28 +0200

    Disabling sound under emscripten for now.

--- renderer/sdl2_renderer.hpp
+++ renderer/sdl2_renderer.hpp
@@ -50,11 +50,15 @@ namespace yagbe
 				accept_image(frame);
 			};
 
-			ctx.apu.configure(_audioSpec.freq, _audioSpec.freq / 60);
+#ifndef __EMSCRIPTEN__
+			ctx.apu.configure(_audioSpec.freq, _audioSpec.samples / 4);
 			ctx.apu.onSamplesReady = [&](auto&& samples)
 			{
 				accept_samples(std::move(samples));
 			};
+#else
+			ctx.apu.configure(_audioSpec.freq, _audioSpec.freq / 60);
+#endif
 		}
 
 		bool frame_drawn()
